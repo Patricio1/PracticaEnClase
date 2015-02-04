0901bed706cbc599355e523cf8bf2fb0908e000f
Patricio1 <apatriciol_@hotmail.com>
2015-02-04 11:55:18 -0500

    hola

---
+++ Repeticion_prueba/Repeticion_prueba/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Repeticion_prueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cajaTexto = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lstResultados = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // cajaTexto
+            // 
+            this.cajaTexto.Location = new System.Drawing.Point(39, 28);
+            this.cajaTexto.Name = "cajaTexto";
+            this.cajaTexto.Size = new System.Drawing.Size(135, 143);
+            this.cajaTexto.TabIndex = 0;
+            this.cajaTexto.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(99, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstResultados
+            // 
+            this.lstResultados.FormattingEnabled = true;
+            this.lstResultados.Location = new System.Drawing.Point(224, 28);
+            this.lstResultados.Name = "lstResultados";
+            this.lstResultados.Size = new System.Drawing.Size(128, 147);
+            this.lstResultados.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 262);
+            this.Controls.Add(this.lstResultados);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cajaTexto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox cajaTexto;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lstResultados;
+    }
+}
+
---
+++ Repeticion_prueba/Repeticion_prueba/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Repeticion_prueba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ingreso(cajaTexto.Text);
+        }
+
+        public void ingreso(String dato)
+        {
+
+            #region N_palabras
+            int i=0;
+            int c = 1;
+            char ca;
+            while(i<=dato.Length-1){
+                ca = dato[i];
+                if(ca==' '|| ca=='\n'||ca=='\t'){
+                    c++;
+                    
+                }
+            i++;
+            }
+         MessageBox.Show("numero de palabras "+c);
+            #endregion
+
+
+            #region Borrar_Especiales
+            String []vector=new String[c];
+            int j = 0;
+            char car;
+            String nuevaCadena="";
+            while(j<=dato.Length-1){
+                car = dato[j];
+                if (!(car == '.' || car == ',' || car == ':' || car == ';'))
+                {
+                    nuevaCadena += car;
+                
+                }
+
+                j++;
+            }
+           MessageBox.Show(nuevaCadena);
+            #endregion
+
+            int k = 0;
+            int l = 0;
+            char ct;
+            String palabra="";
+            while(k<=nuevaCadena.Length-1){
+                ct=nuevaCadena[k];
+                if(ct==' '|| ct=='\n'){   
+                    vector[l] = palabra;
+                    l++;
+                    palabra = "";
+                   
+                    
+
+                    //ct++;
+                }
+                int s = vector.Length - 1;
+                //if (vector.i)
+                //{
+                //    vector[l] = palabra;
+                //}
+                palabra += ct;
+
+            k++;
+            }
+
+            int o = 0;
+            int h = vector.Length;
+            while(o<=vector.Length-1){
+                MessageBox.Show(vector[o]);
+              //  lstResultados.Items.Add(vector[o]);
+            o++;
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
